b955c6576f8057eed8da41de925cb2d37a7d1ef8
mohamed3b3az <mohamedabdelaziem96@gmail.com>
2024-07-10 04:35:06 +0300

    Finish the main logic of javascript

--- Guess-my-name/script.js
+++ Guess-my-name/script.js
@@ -1,4 +1,56 @@
 'use strict'
 // document.querySelector('.number').value=5; --->Acces a value
 // document.querySelector('.guess').textcontent="something"; --->Acces a string
-//
+
+const secretNumber=Math.trunc((Math.random())*20)+1;
+const maxnumber=20;
+document.querySelector('.number1').textContent=Math.trunc(maxnumber/2);
+
+document.querySelector('#check').addEventListener('click',function()
+{
+    document.querySelector('.number').textContent=(document.querySelector('#guess').value);
+
+    const guess=Number(document.querySelector('#guess').value);
+     if(guess===secretNumber)
+    {
+        document.querySelector('.message').textContent="Correct Answer🎊";
+        document.querySelector('.number2').textContent=document.querySelector('.number1').textContent;
+        document.querySelector('.number1').textContent=maxnumber;
+    }
+    else if(guess<secretNumber)
+    {
+        if(guess<0.5*secretNumber)
+        {
+            document.querySelector('.message').textContent="Too Low📉";
+            let minus=document.querySelector('.number1').textContent;
+            document.querySelector('.number1').textContent=minus-1;
+        }
+         if(guess>0.5*secretNumber)
+        {
+           document.querySelector('.message').textContent="Too close📉";
+           let minus=document.querySelector('.number1').textContent;
+           document.querySelector('.number1').textContent=minus-1;
+        }
+    }
+    else if(guess>secretNumber)
+    {
+        if(guess>2*secretNumber)
+        {
+            document.querySelector('.message').textContent="Too High📈";
+            let minus=document.querySelector('.number1').textContent;
+            document.querySelector('.number1').textContent=minus-1;
+        }
+        if(guess<2*secretNumber)
+        {
+            document.querySelector('.message').textContent="Too close📈";
+            let minus=document.querySelector('.number1').textContent;
+            document.querySelector('.number1').textContent=minus-1;
+        }
+    }
+    if(document.querySelector('.number1').textContent==0)
+    {
+        document.querySelector('.message').textContent="You Lost😢";
+        document.querySelector('.number').textContent=secretNumber;
+    }
+    
+})
